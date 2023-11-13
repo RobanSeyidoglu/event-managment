8925dc90ee42d762d5c4fbbdc6584cafe48698bc
Roban Seyidoğlu <robanseyidoglu@gmail.com>
2023-11-13 20:31:34 +0300

    u

--- postcss.config.js
+++
@@ -1,6 +0,0 @@
-export const postcssConfig = {
-  plugins: {
-    "@tailwindcss/jit": {},
-    autoprefixer: {},
-  },
-};
--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,13 @@
 
-import { useState } from "react";
+import './App.css'
 
-import { TasksProvider } from "./context/EventsContext";
-import Header from "./components/Header/Header";
-import Sidebar from "./components/Sidebar/Sidebar";
-import TaskBoard from "./components/EventBoard/EventBoard";
-
-import styles from "./App.module.css";
-
-const App = () => {
-  const [selectedList, setSelectedList] = useState("inbox");
-  const [searchedTask, setSearchedTask] = useState("");
-  const [isSidebarHidden, setIsSidebarHidden] = useState(false);
-
-  const taskLists = [
-    { value: "noDate", label: "" },
-    { value: "inbox", label: "Inbox", icon: "inbox" },
-    { value: "important", label: "Important", icon: "important" },
-    { value: "overdue", label: "Overdue", icon: "history" },
-    { value: "today", label: "Today", icon: "today" },
-    { value: "tomorrow", label: "Tomorrow", icon: "pushpin" },
-    {
-      value: "nextSevenDays",
-      label: "Next 7 Days",
-      icon: "star",
-    },
-    { value: "upcoming", label: "Upcoming", icon: "hourglass" },
-  ];
+function App() {
 
   return (
-    <TasksProvider>
-      <div className={styles.App}>
-        <Header
-          selectedList={selectedList}
-          setSelectedList={setSelectedList}
-          searchedTask={searchedTask}
-          setSearchedTask={setSearchedTask}
-          toggleSidebar={() => setIsSidebarHidden(!isSidebarHidden)}
-        />
-        <div className={styles.TaskContent}>
-          <Sidebar
-            isSidebarHidden={isSidebarHidden}
-            taskLists={taskLists}
-            selectedList={selectedList}
-            setSelectedList={setSelectedList}
-          />
-          <TaskBoard
-            taskLists={taskLists}
-            selectedList={selectedList}
-            searchedTask={searchedTask}
-          />
-        </div>
-      </div>
-    </TasksProvider>
-  );
-};
+    <>
+     <h1>hi</h1>
+    </>
+  )
+}
 
-export default App;
+export default App
