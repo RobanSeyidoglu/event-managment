26876796989ee2e8ebb33258dc8acc0634b3f829
Roban Seyidoğlu <robanseyidoglu@gmail.com>
2023-11-13 20:38:27 +0300

    add missing packages and fix styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,58 @@
+import { useState } from "react";
 
-import './App.css'
+import { TasksProvider } from "./context/tasksContext.jsx";
+import Header from "./components/Header/Header";
+import Sidebar from "./components/Sidebar/Sidebar";
+import TaskBoard from "./components/TaskBoard/TaskBoard";
 
-function App() {
+import styles from "./App.module.css";
 
-  return (
-    <>
-     <h1>hi</h1>
-    </>
-  )
-}
+const App = () => {
+    const [selectedList, setSelectedList] = useState("inbox");
+    const [searchedTask, setSearchedTask] = useState("");
+    const [isSidebarHidden, setIsSidebarHidden] = useState(false);
 
-export default App
+    const taskLists = [
+        { value: "noDate", label: "" },
+        { value: "inbox", label: "Inbox", icon: "inbox" },
+        { value: "important", label: "Important", icon: "important" },
+        { value: "overdue", label: "Overdue", icon: "history" },
+        { value: "today", label: "Today", icon: "today" },
+        { value: "tomorrow", label: "Tomorrow", icon: "pushpin" },
+        {
+            value: "nextSevenDays",
+            label: "Next 7 Days",
+            icon: "star",
+        },
+        { value: "upcoming", label: "Upcoming", icon: "hourglass" },
+    ];
+
+    return (
+        <TasksProvider>
+            <div className={styles.App}>
+                <Header
+                    selectedList={selectedList}
+                    setSelectedList={setSelectedList}
+                    searchedTask={searchedTask}
+                    setSearchedTask={setSearchedTask}
+                    toggleSidebar={() => setIsSidebarHidden(!isSidebarHidden)}
+                />
+                <div className={styles.TaskContent}>
+                    <Sidebar
+                        isSidebarHidden={isSidebarHidden}
+                        taskLists={taskLists}
+                        selectedList={selectedList}
+                        setSelectedList={setSelectedList}
+                    />
+                    <TaskBoard
+                        taskLists={taskLists}
+                        selectedList={selectedList}
+                        searchedTask={searchedTask}
+                    />
+                </div>
+            </div>
+        </TasksProvider>
+    );
+};
+
+export default App;
